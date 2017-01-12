04d5e1432d91b2cc6c1fc90a45d1cfb7f771a354
Tommy Crews <crewst@student.swosu.edu>
2017-01-12 09:34:05 -0600

    Generated project base files.

---
+++ Radar/Radar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Radar
+//
+//  Created by Thomas Crews on 1/11/17.
+//  Copyright © 2017 Thomas Crews. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
